1c1359b4984d3fa050c65ed13550dabd7b8239a4
Fernando Paladini <fnpaladini@gmail.com>
2015-11-23 00:17:28 -0200

    First commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+Contagem Regressiva UFSC 0.1
+==================
+
+Extensão para o Chrome que exibe a contagem regressiva de dias para o Vestibular UFSC 2016 - apenas uma pequena ferramenta para os vestibulandos da UFSC 2016. 
+
+Boa prova!
+
+Sobre e Colaboração
+==================
+Esta extensão foi desenvolvida por [Fernando Paladini](http://fpaladini.blogspot.com), mas a ideia surgiu de [Filipe Ronzani](https://www.facebook.com/fgronzani), vestibulando de Medicina que solicitou uma extensão similar a essa. Essa extensão foi criada em cerca de 1 hora, portanto não tive quase nenhum trabalho (pois me baseei [nessa extensão](https://github.com/paladini/QuantoCusta1BitcoinHoje) que já havia desenvolvido antigamente). 
+
+Sinta-se à vontade para criar uma extensão para o vestibular da sua cidade baseado nesse projeto open-source. 
+
+Licença
+==================
+Este projeto é licenciado sob os termos da MIT, conforme pode ser visto no arquivo LICENSE neste repositório.
+
+
---
+++ chrome/js/background.js
@@ -0,0 +1,50 @@
+function daysInMonth(month,year) {
+    return new Date(year, month, 0).getDate();
+}
+
+function calculate_remaining_days() {
+	var testDate = new Date(2015, 11, 12);
+
+	var today = new Date();
+	var remaining_months = (testDate.getMonth()+1) - (today.getMonth()+1);
+
+	var remaining_days = (daysInMonth(today.getMonth()+1, today.getFullYear()) - today.getDate());
+	for(var i = 1; i <= remaining_months; i++) {
+		if (i == remaining_months) {
+			remaining_days += testDate.getDate();
+		} else {
+			remaining_days += daysInMonth((testDate.getMonth()+1)+i);
+		}
+	}
+
+	return remaining_days;
+}
+
+// Create the alarm and update the badge text.
+function onAlarm(alarm) {
+
+	var remaining_days = calculate_remaining_days();
+	chrome.browserAction.setBadgeBackgroundColor({color: "#CC0000"});
+	chrome.browserAction.setBadgeText({text: remaining_days.toString() });
+	chrome.browserAction.setTitle({title: "Faltam " + remaining_days + " dias para o Vestibular UFSC 2016."});
+
+	// Calculating tomorrow
+	var tomorrow = new Date();
+	tomorrow.setDate(tomorrow.getDate()+1);
+	tomorrow.setHours(0);
+	tomorrow.setMinutes(1);
+	tomorrow.setSeconds(0);
+	tomorrow.setMilliseconds(0);
+	chrome.alarms.create("next_alarm", { when: parseFloat(tomorrow.toString()) });
+}
+
+// Trigger the alarm.
+chrome.alarms.onAlarm.addListener(onAlarm);
+
+// Opening webpage when clicking in the extension button.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.create({'url': "http://vestibular2016.ufsc.br"});
+});
+
+// Call the method
+onAlarm();
